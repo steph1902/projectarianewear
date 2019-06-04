c4aa19e1e8fd09cbc11f37822e961767aee1a2fe
steph1902 <stephanus1902@gmail.com>
2019-06-04 22:52:57 +0700

    added size seeder
    added size seeder

---
+++ database/seeds/ProductsSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProductsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $this->call([
+            ProductsTableSeeder::class,
+            ColorsTableSeeder::class,
+            SizeTableSeeder::class,
+            ImageTableSeeder::class
+        ]);
+    }
+}
---
+++ database/seeds/SizeTableSeeder.php
@@ -0,0 +1,257 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SizeTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+
+            DB::table('sizes')->insert([
+                'product_name' => 'ABBY TOP',
+                'size_name' => 'ALL SIZE'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'AGATHA DRESS',
+                'size_name' => 'S'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'AGATHA DRESS',
+                'size_name' => 'M'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'AGATHA DRESS',
+                'size_name' => 'L'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'ALEEYAH DRESS',
+                'size_name' => 'ALL SIZE'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'ALEXA TOP',
+                'size_name' => 'ALL SIZE'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'ALICE VEST',
+                'size_name' => 'ALL SIZE'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'ALINA',
+                'size_name' => 'ALL SIZE'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'ARA PLEATS TOP',
+                'size_name' => 'ALL SIZE'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'ARIANE TANKTOP',
+                'size_name' => 'S'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'ARIANE TANKTOP',
+                'size_name' => 'M'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'BELLA TOP',
+                'size_name' => 'M'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'BIANCA TANK TOP',
+                'size_name' => 'S'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'BIANCA TANK TOP',
+                'size_name' => 'M'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'CAIL DRESS',
+                'size_name' => 'ALL SIZE'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'CELINE DRESS',
+                'size_name' => 'ALL SIZE'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'CHLOE',
+                'size_name' => 'ALL SIZE'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'CLARA OUTER',
+                'size_name' => 'ALL SIZE'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'DOROTHY TANK TOP',
+                'size_name' => 'ALL SIZE'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'EBONY',
+                'size_name' => 'ALL SIZE'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'EVA TOP',
+                'size_name' => 'ALL SIZE'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'FRISKA PLEATS DRESS',
+                'size_name' => 'ALL SIZE'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'INDY OUTER',
+                'size_name' => 'ALL SIZE'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'JOLLYN',
+                'size_name' => 'ALL SIZE'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'KIM JUMPSUIT',
+                'size_name' => 'ALL SIZE'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'KOURTNEY DRESS',
+                'size_name' => 'ALL SIZE'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'LORRA DRESS',
+                'size_name' => 'ALL SIZE'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'LUNA VEST',
+                'size_name' => 'ALL SIZE'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'LYN TOP',
+                'size_name' => 'ALL SIZE'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'MARSYA TOP',
+                'size_name' => 'S'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'MARSYA TOP',
+                'size_name' => 'M'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'MARZA TANK TOP',
+                'size_name' => 'ALL SIZE'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'MEZZA TOP',
+                'size_name' => 'ALL SIZE'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'MIRA TOP',
+                'size_name' => 'ALL SIZE'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'NATASHA SETS',
+                'size_name' => 'S'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'NATASHA SETS',
+                'size_name' => 'M'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'NATASHA SETS',
+                'size_name' => 'L'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'SHELBY TOP',
+                'size_name' => 'ALL SIZE'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'SURI TOP',
+                'size_name' => 'ALL SIZE'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'VEBY PANTS',
+                'size_name' => 'ALL SIZE'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'VICKY PANTS',
+                'size_name' => 'S'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'VICKY PANTS',
+                'size_name' => 'M'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'VICKY PANTS',
+                'size_name' => 'L'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'VIVIAN TOP',
+                'size_name' => 'ALL SIZE'
+            ]);
+
+            DB::table('sizes')->insert([
+                'product_name' => 'YOONA PLEADS DRESS',
+                'size_name' => 'ALL SIZE'
+            ]);
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
